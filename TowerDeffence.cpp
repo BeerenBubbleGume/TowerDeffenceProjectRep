a5e337d01419dc103ab72bc3736b407da4cd3419
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2021-07-02 19:21:13 +0500

    Delete TowerDeffence.cpp

--- TowerDeffence.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TowerDeffence.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, TowerDeffence, "TowerDeffence");
-
-DEFINE_LOG_CATEGORY(LogTowerDeffence)
